a97eab8cae26e6abf67d36fe2e3c716341b10578
0andme <yu00mi97@gmail.com>
2023-02-09 08:54:25 +0900

    장바구니 실습 vue

---
+++ vue/my-cart/src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="list">
+    <li v-for="(chip, i) in chipsData" :key="chip.title">
+      <img :src="require(`@/assets/${chip.img}`)" />
+      <strong>{{ chip.title }}</strong>
+      <span>{{ chip.desc }}</span>
+      <b>{{ chip.price.toLocaleString() }}원</b>
+      <button @click="addItem(i)">구입하기</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    chipsData: Array,
+  },
+  methods: {
+    addItem(price) {
+      this.$emit("add-item", Number(price));
+    },
+  },
+};
+</script>
