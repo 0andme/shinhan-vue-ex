b2de899baeecf29f8df8c736789cdf5e6de9c68e
0andme <yu00mi97@gmail.com>
2023-02-09 09:27:31 +0900

    영화 리스트 분리

--- vue/my-movie/src/App.vue
+++ vue/my-movie/src/App.vue
@@ -11,55 +11,14 @@
       </li>
     </ul>
     <!-- 영화 리스트 -->
-    <ol class="list">
-      <li v-for="(movie, i) in movies" :key="movie.name">
-        <a href="">
-          <h4 class="hide">{{ movie.name }}</h4>
-          <!-- <span :class="'hide rate rate'+ movie.rate[0]"
-              >{{movie.rate[1]}}</span
-            > -->
-          <span :class="['hide', 'rate', `rate${movie.rate[0]}`]">{{
-            movie.rate[1]
-          }}</span>
-          <strong class="rank">{{ i + 1 }}</strong>
-          <img
-            class="poster"
-            :src="require(`@/assets/${movie.src}`)"
-            :alt="movie.name"
-          />
-          <!-- 예매순 -->
-          <span v-if="movContext === 'ticketing'" class="desc">
-            <i class="reserve_title">예매율</i>
-            <em class="reserv_num">{{ movie.ticketing }}%</em>
-          </span>
-          <!-- 평점순 -->
-          <span v-else-if="movContext === 'score'" class="desc">
-            <i class="star"><b :style="{width: movie.score * 10 + '%'}"></b></i>
-            <em class="score">{{ movie.score }}</em>
-          </span>
-          <!-- 개봉일순 -->
-          <span v-else-if="movContext === 'openDate'" class="desc">
-            <i class="open_title">{{ movie.openDate }}</i>
-            <em class="open_num">개봉</em>
-          </span>
-          <!-- 주말관객순 -->
-          <span v-else-if="movContext === 'week'" class="desc">
-            <i class="week_title">주말관객수</i>
-            <em class="week_num">{{ movie.week }}명</em>
-          </span>
-          <!-- 다운로드수 -->
-          <span v-else-if="movContext === 'sale'" class="desc">
-            <i class="sale_title">판매율</i>
-            <em class="sale_num">{{ movie.sale }} %</em>
-          </span>
-        </a>
-      </li>
-    </ol>
+    <MovieList :movies="movies" :mov-context="movContext"></MovieList>
   </div>
 </template>
 
 <script>
+import MovieList from "./components/MovieList.vue";
 export default {
+  components: {MovieList},
   data() {
     return {
       // listCate: {
