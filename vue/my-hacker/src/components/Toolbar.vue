bc4c7e5ec2fbb135c66bce479b263c6b9bc856de
0andme <yu00mi97@gmail.com>
2023-02-10 10:08:18 +0900

    css 추가

--- vue/my-hacker/src/components/Toolbar.vue
+++ vue/my-hacker/src/components/Toolbar.vue
@@ -10,4 +10,32 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+header {
+  position: relative;
+  padding: 20px 30px;
+  margin-bottom: 30px;
+  background: black;
+  font-size: 24px;
+  border-bottom: 8px solid lime;
+}
+header h1 {
+  position: absolute;
+  color: white;
+  top: 21px;
+  right: 30px;
+  font-size: 18px;
+}
+header a {
+  color: white;
+  text-decoration: none;
+  padding-bottom: 3px;
+}
+header a + a {
+  margin-left: 20px;
+}
+header .router-link-active {
+  color: coral;
+  border-bottom: 3px solid coral;
+}
+</style>
