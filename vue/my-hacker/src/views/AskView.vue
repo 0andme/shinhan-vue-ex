01f56ca85df485d5046b7b805e2d1e5f166404b9
0andme <yu00mi97@gmail.com>
2023-02-10 12:59:40 +0900

    ask jobs 뷰 추가

--- vue/my-hacker/src/views/AskView.vue
+++ vue/my-hacker/src/views/AskView.vue
@@ -1,7 +1,34 @@
-<template><div>Ask</div></template>
+<template>
+  <div>
+    <h2>Ask</h2>
+    <ul class="list">
+      <li v-for="news in list" :key="news.id">
+        <b>{{ news.time_ago }}</b>
+        <span> by {{ news.user }}</span
+        ><br />
+        <a :href="news.url"
+          >{{ news.title }}<i>({{ news.comments_count }})</i></a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    axios.get("https://api.hnpwa.com/v0/ask/1.json").then((res) => {
+      this.list = res.data;
+    });
+  },
+};
 </script>
 
 <style></style>
--- vue/my-hacker/src/views/JobsView.vue
+++ vue/my-hacker/src/views/JobsView.vue
@@ -1,7 +1,33 @@
-<template><div>jobs</div></template>
+<template>
+  <div>
+    <h2>jobs</h2>
+    <ul class="list">
+      <li v-for="news in list" :key="news.id">
+        <b>{{ news.time_ago }}</b>
+        <br />
+        <a :href="news.url"
+          >{{ news.title }} <i>({{ news.comments_count }})</i></a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    axios.get("https://api.hnpwa.com/v0/jobs/1.json").then((res) => {
+      this.list = res.data;
+    });
+  },
+};
 </script>
 
 <style></style>
