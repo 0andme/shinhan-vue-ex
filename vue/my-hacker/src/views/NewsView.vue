5ac7caba279a9cfad02887d5ad4f930f7f931b06
0andme <yu00mi97@gmail.com>
2023-02-10 10:33:28 +0900

    api로부터 data 받아 화면에 표시
    - data 받아 화면에 출력
    - 스타일 추가

--- vue/my-hacker/src/views/NewsView.vue
+++ vue/my-hacker/src/views/NewsView.vue
@@ -1,7 +1,62 @@
-<template><div>news</div></template>
+<template>
+  <div>
+    <h2>news</h2>
+    <ul class="list">
+      <li v-for="news in list" :key="news.id">
+        <b>{{ news.time_ago }}</b>
+        <span> by {{ news.user }}</span
+        ><br />
+        <a :href="news.url"
+          >{{ news.title }} <i>({{ news.comments_count }})</i></a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    axios.get("https://api.hnpwa.com/v0/news/1.json").then((res) => {
+      this.list = res.data;
+    });
+  },
+};
 </script>
 
-<style></style>
+<style>
+.list li {
+  padding-left: 10px;
+  list-style-type: none;
+}
+.list li + li {
+  margin-top: 20px;
+}
+.list li > a {
+  text-decoration: none;
+  font-size: 22px;
+}
+.list li > b {
+  font-style: normal;
+  font-size: 12px;
+  color: #999;
+}
+.list li span {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  color: #999;
+}
+.list li i {
+  font-style: normal;
+  font-size: 12px;
+  color: cornflowerblue;
+}
+</style>
